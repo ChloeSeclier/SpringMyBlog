970db2c9ba54015b3835fea5508786659cdda225
Chloe Seclier <seclier.chloe@gmail.com>
2025-05-09 10:22:40 +0200

    first commit

---
+++ src/main/java/org/wildecodeschool/MyBlog/MonPremierBlogApplication.java
@@ -0,0 +1,13 @@
+package org.wildecodeschool.MyBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonPremierBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonPremierBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wildecodeschool/MyBlog/MonPremierBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wildecodeschool.MyBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonPremierBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
